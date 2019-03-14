58b4e5686ea02fe114195d1ca0b6c0de185093e8
Rein Chau <rein.chau@firstidea.vn>
2019-03-14 17:58:10 +0700

    Added linkPackages.js to auto create symlinks from libs packages to root node_modules/@micro-fleet;

--- scripts/definition.js
+++ scripts/definition.js
@@ -1,17 +1,20 @@
-const fs = require('fs-extra');
-const path = require('path');
+const fs = require('fs-extra')
+const path = require('path')
 
-const dts = require('ts-declarator').default;
+const dts = require('ts-declarator').default
 
-const log = require('./common');
+const log = require('./common')
 
 
+/**
+ * Create TypeScript definition file (.d.ts) for a package.
+ */
 async function definition() {
-	log.bold('> definition.js');
+	log.bold('> definition.js')
 
-	const CWD = process.cwd();
-	const PKG = await fs.readJson(path.join(CWD, 'package.json'));
-	const DEF_FILE = path.join(CWD, 'typings', 'app.d.ts');
+	const CWD = process.cwd()
+	const PKG = await fs.readJson(path.join(CWD, 'package.json'))
+	const DEF_FILE = path.join(CWD, 'typings', 'app.d.ts')
 	const config = {
 		name: `${PKG.name}/dist`,
 		project: CWD,
@@ -19,24 +22,24 @@ async function definition() {
 		sendMessage: console.log,
 		externs: ['./global.d.ts'],
 		exclude: ['test/**/*.*']
-	};
+	}
 
-	await fs.remove(DEF_FILE);
-	await dts(config);
+	await fs.remove(DEF_FILE)
+	await dts(config)
 
-	const content = await fs.readFile(DEF_FILE, 'utf8');
+	const content = await fs.readFile(DEF_FILE, 'utf8')
 
-	const newContent = content.replace(/([\t\f\v]*)private(.*);[\r\n]*/g, '')
+	const newContent = content.replace(/([\t\f\v]*)private(.*)[\r\n]*/g, '')
 		.replace(/\/src\//g, '/dist/')
 		.replace(/\/dist\/app\/index'/g, "'")
 
-	await fs.writeFile(DEF_FILE, newContent);
-	log.success('Definition generated');
+	await fs.writeFile(DEF_FILE, newContent)
+	log.success('Definition generated')
 }
 
 // If required by another file
 if (module.parent) {
-	module.exports = definition;
+	module.exports = definition
 } else { // If executed from terminal
-	definition();
+	definition()
 }
